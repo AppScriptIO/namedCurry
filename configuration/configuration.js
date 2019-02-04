9ef746b63fee8dab42547985e8138f066d3fdcaa
Dr. Safi <sfmissive@gmail.com>
2019-02-04 17:32:09 +0200

    ⚗ Refactor dev dependencies and script configurations.

--- configuration/configuration.js
+++ configuration/configuration.js
@@ -1,36 +1,9 @@
 const path = require('path')
-const projectPath = "/project";
-const appDeploymentLifecyclePath = path.dirname( require.resolve(`@dependency/appDeploymentLifecycle/package.json`) ) 
+const { script } = require('./script.config.js')
 
 module.exports = {
     directory: {
-        projectPath,
-        application: {
-            hostAbsolutePath: path.resolve(`${__dirname}/../..`),
-            containerAbsolutePath: `${projectPath}/application`
-        }
+        root: path.resolve(`${__dirname}/..`),
     },
-    script: {
-        hostMachine: [
-            { // example for module path
-                type: 'module',
-                key: 'containerManager',
-                path: './setup/node_modules/@dependency/appDeploymentManager/setup/script/bin/containerManager.js'
-            },
-            {
-                type: 'directory',
-                path: './setup/script/hostMachine' // relative to applicaiton repository root.
-            }
-        ],
-        container: [ // entrypoint configuration map, paths are relative to external app.
-            {
-                key: 'test',
-                path: `${appDeploymentLifecyclePath}/containerScript/test`,
-            },
-            {
-                key: 'sleep',
-                path: `${appDeploymentLifecyclePath}/containerScript/sleep`,
-            }
-        ]
-    }
+    script
 }
---
+++ configuration/script.config.js
@@ -0,0 +1,17 @@
+const path = require('path')
+const resolvedModule = {
+    get deploymentScript() { return path.dirname( require.resolve(`@dependency/deploymentScript/package.json`) )  },
+}
+
+module.exports = {
+    script: [
+        {
+            type: 'directory',
+            path: `${resolvedModule.deploymentScript}/script`,
+        },
+        {
+            type: 'directory', 
+            path: './script'
+        }
+    ]
+}
