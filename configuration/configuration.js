208b7226f52d1b0870e8d2fe5eeb9f60d8f18fbb
Safi <sfmissive@gmail.com>
2019-05-07 03:22:25 +0300

    ⚗ Refactor to allow manual building and releasing of package.

--- configuration/configuration.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-const { script } = require('./script.config.js')
-
-module.exports = {
-    directory: {
-        root: path.resolve(`${__dirname}/..`),
-    },
-    script
-}
