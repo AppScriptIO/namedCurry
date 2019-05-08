f0c22dccaa1a20751b5330db48b6699f5a949f37
Dr. Safi <sfmissive@gmail.com>
2019-05-08 05:43:30 +0300

    build

--- source/script.js
+++ source/script.js
@@ -1,3 +1,11 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.curryNamed = curryNamed;
+exports.curryNamedInvokeManually = curryNamedInvokeManually;
+
 /**
  * Source: https://github.com/gunar/ncurry 
  *  Allows for currying on named parameters i.e. { param1: '', param2: ''}
@@ -6,27 +14,22 @@
  * Partially apply a function with dynamic number of arguments.
  */
 // IMPORTANT: take notice of the way "namedCurry" works, if all required arguments are passed then it will execute the function not always the desired behavior.
- 
-export function curryNamed (keys, fn) {
-    return function helper (cache) {
-        return function (...objects) {
-            const incoming = Object.assign({}, ...objects)
-            const argsOverridden = includesSome(Object.keys(cache), Object.keys(incoming))
-            const error = `The following arguments were overidden: ${argsOverridden}`
-            if (argsOverridden.length > 0) throw Error(error)
-            const obj = Object.assign({}, cache, ...objects)
-            return includesEvery(keys, Object.keys(obj)) ? fn(obj) : helper(obj)
-        }
-    }({})
-}
+function curryNamed(keys, fn) {
+  return function helper(cache) {
+    return function (...objects) {
+      const incoming = Object.assign({}, ...objects);
+      const argsOverridden = includesSome(Object.keys(cache), Object.keys(incoming));
+      const error = `The following arguments were overidden: ${argsOverridden}`;
+      if (argsOverridden.length > 0) throw Error(error);
+      const obj = Object.assign({}, cache, ...objects);
+      return includesEvery(keys, Object.keys(obj)) ? fn(obj) : helper(obj);
+    };
+  }({});
+} // TODO: check todo list in documentation folder.
 
-// TODO: check todo list in documentation folder.
-export function curryNamedInvokeManually() {}
 
-const includesEvery = (mandatory, received) =>
-    mandatory.every(key =>
-        received.includes(key))
+function curryNamedInvokeManually() {}
 
-const includesSome = (mandatory, received) =>
-    mandatory.filter(key =>
-        received.includes(key))
+const includesEvery = (mandatory, received) => mandatory.every(key => received.includes(key));
+
+const includesSome = (mandatory, received) => mandatory.filter(key => received.includes(key));
